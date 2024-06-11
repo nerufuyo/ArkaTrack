514fa1c96cdc64823d3bf6e1fef3c2ce437b9fab
Listyo Adi <listyoadipamungkas1401@gmail.com>
2024-06-11 09:23:01 +0700

    add category widget, quick info and attendance widgets

---
+++ lib/presentation/screens/dashboard/widgets/category_list_widget.dart
@@ -0,0 +1,59 @@
+import 'package:arkatrack/style/color.dart';
+import 'package:arkatrack/style/typography.dart';
+import 'package:arkatrack/utils/constant.dart';
+import 'package:arkatrack/utils/extension.dart';
+import 'package:flutter/material.dart';
+
+class CategoryListsWidget extends StatelessWidget {
+  const CategoryListsWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: GridView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: categoriesList.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {},
+            splashFactory: InkRipple.splashFactory,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: AppColors.grey,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  padding: const EdgeInsets.all(8),
+                  child: Image.asset(
+                    categoriesList[index]['icon'] as String,
+                    width: 48,
+                    height: 48,
+                  ),
+                ),
+                AppTypography(
+                  text: categoriesList[index]['label'] as String,
+                  fontSize: 12,
+                  color: AppColors.black,
+                ),
+              ],
+            ).withVerticalSpacing(4),
+          );
+        },
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          crossAxisSpacing: 4,
+          mainAxisSpacing: 4,
+        ),
+      ),
+    );
+  }
+}
