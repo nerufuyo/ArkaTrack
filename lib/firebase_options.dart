f7461288c9c457083dcbb46c42bd10ce70dd7603
Listyo Adi <listyoadipamungkas1401@gmail.com>
2024-12-26 11:07:02 +0700

    ✨feature: add `dotenv` for hide key

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -13,21 +13,25 @@ import 'package:flutter/foundation.dart'
 /// await Firebase.initializeApp(
 ///   options: DefaultFirebaseOptions.currentPlatform,
 /// );
-/// ```
+/// ```import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      return web;
+      return _web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
-        return android;
+        return _android;
       case TargetPlatform.iOS:
-        return ios;
+        return _ios;
       case TargetPlatform.macOS:
-        return macos;
+        return _macos;
       case TargetPlatform.windows:
-        return windows;
+        return _windows;
       case TargetPlatform.linux:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for linux - '
@@ -40,49 +44,49 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyALBxIjYqbuwKKC0Vm5d9uFCZKOXigTeKY',
-    appId: '1:235892732833:web:6320647af9e8237b1891e0',
-    messagingSenderId: '235892732833',
-    projectId: 'arkatrack',
-    authDomain: 'arkatrack.firebaseapp.com',
-    storageBucket: 'arkatrack.firebasestorage.app',
-    measurementId: 'G-5K0V0X90CW',
-  );
+  static FirebaseOptions get _web => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_WEB_API_KEY']!,
+        appId: dotenv.env['FIREBASE_WEB_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_WEB_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_WEB_PROJECT_ID']!,
+        authDomain: dotenv.env['FIREBASE_WEB_AUTH_DOMAIN']!,
+        storageBucket: dotenv.env['FIREBASE_WEB_STORAGE_BUCKET']!,
+        measurementId: dotenv.env['FIREBASE_WEB_MEASUREMENT_ID']!,
+      );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCQiZUsNZAWd2YW-acJ96LkGYUVov5t4MA',
-    appId: '1:235892732833:android:a49f22b4ea7b3ff31891e0',
-    messagingSenderId: '235892732833',
-    projectId: 'arkatrack',
-    storageBucket: 'arkatrack.firebasestorage.app',
-  );
+  static FirebaseOptions get _android => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY']!,
+        appId: dotenv.env['FIREBASE_ANDROID_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_ANDROID_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_ANDROID_PROJECT_ID']!,
+        storageBucket: dotenv.env['FIREBASE_ANDROID_STORAGE_BUCKET']!,
+      );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyDMTUkspICujYxcv9FIGmwrihn-MiH0UvM',
-    appId: '1:235892732833:ios:2b38c0230c19e5431891e0',
-    messagingSenderId: '235892732833',
-    projectId: 'arkatrack',
-    storageBucket: 'arkatrack.firebasestorage.app',
-    iosBundleId: 'com.example.arkatrack',
-  );
+  static FirebaseOptions get _ios => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_IOS_API_KEY']!,
+        appId: dotenv.env['FIREBASE_IOS_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_IOS_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_IOS_PROJECT_ID']!,
+        storageBucket: dotenv.env['FIREBASE_IOS_STORAGE_BUCKET']!,
+        iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID']!,
+      );
 
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyDMTUkspICujYxcv9FIGmwrihn-MiH0UvM',
-    appId: '1:235892732833:ios:2b38c0230c19e5431891e0',
-    messagingSenderId: '235892732833',
-    projectId: 'arkatrack',
-    storageBucket: 'arkatrack.firebasestorage.app',
-    iosBundleId: 'com.example.arkatrack',
-  );
+  static FirebaseOptions get _macos => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_MACOS_API_KEY']!,
+        appId: dotenv.env['FIREBASE_MACOS_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_MACOS_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_MACOS_PROJECT_ID']!,
+        storageBucket: dotenv.env['FIREBASE_MACOS_STORAGE_BUCKET']!,
+        iosBundleId: dotenv.env['FIREBASE_MACOS_BUNDLE_ID']!,
+      );
 
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyALBxIjYqbuwKKC0Vm5d9uFCZKOXigTeKY',
-    appId: '1:235892732833:web:5cb81f19120edd191891e0',
-    messagingSenderId: '235892732833',
-    projectId: 'arkatrack',
-    authDomain: 'arkatrack.firebaseapp.com',
-    storageBucket: 'arkatrack.firebasestorage.app',
-    measurementId: 'G-CLW2098V3H',
-  );
+  static FirebaseOptions get _windows => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_WINDOWS_API_KEY']!,
+        appId: dotenv.env['FIREBASE_WINDOWS_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_WINDOWS_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_WINDOWS_PROJECT_ID']!,
+        authDomain: dotenv.env['FIREBASE_WINDOWS_AUTH_DOMAIN']!,
+        storageBucket: dotenv.env['FIREBASE_WINDOWS_STORAGE_BUCKET']!,
+        measurementId: dotenv.env['FIREBASE_WINDOWS_MEASUREMENT_ID']!,
+      );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:arkatrack/common/routes/route.dart';
 import 'package:arkatrack/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     name: 'ArkAtrack',
     options: DefaultFirebaseOptions.currentPlatform,
